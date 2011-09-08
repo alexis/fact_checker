50cb7fa8b85631f423394c2fc2433792018c43f8
Alexey Smolianinov <alexisowl@gmail.com>
2011-09-08 17:37:50 +0400

    sketch of the FactChecker mixin (fixing previous commit)

--- lib/fact_checker.rb
+++ lib/fact_checker.rb
@@ -1,4 +1,37 @@
 require "fact_checker/version"
 require "fact_checker/base"
 
-class FactChecker::Error < StandardError; end
+module FactChecker
+
+  def self.included(klass)
+    klass.extend(ClassMethods)
+    klass.instance_variable_set('@fact_checker', FactChecker::Base.new)
+  end
+
+  module ClassMethods
+
+    def inherited(child)
+      child.instance_variable_set('@fact_checker', @fact_checker.dup)
+    end
+
+    def define_fact(*options)
+      @fact_checker.define_fact(options)
+    end
+
+  end
+
+  def step_acomplished?(step)
+  end
+
+  def step_possible?(step)
+  end
+
+  def steps
+  end
+
+  def possible_steps(step)
+  end
+
+  def acomplished_steps(step)
+  end
+end
