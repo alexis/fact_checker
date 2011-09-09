65e9a796984eee5d826ba4991cd3ca3a5ebef99b
Ivan Povalyukhin <ipoval@ya.ru>
2011-09-09 07:39:37 -0700

    shortened the code to define instance methods for FactChecker

--- lib/fact_checker.rb
+++ lib/fact_checker.rb
@@ -7,12 +7,6 @@ require "fact_checker/base"
 module FactChecker
   def self.included(base)
     base.extend ClassMethods
-
-    base.class_eval do
-      [ :fact_on?, :fact_can?, :facts, :facts_on, :facts_can ].each do |m|
-        define_method(m) { |fact| self.class.fact_checker.send(m, self, fact) }
-      end
-    end
   end
 
   module ClassMethods
@@ -23,5 +17,9 @@ module FactChecker
     def fact_checker
       @fact_checker ||= FactChecker::Base.new
     end
+  end
+
+  [ :fact_on?, :fact_can?, :facts, :facts_on, :facts_can ].each do |m|
+    define_method(m) { |fact| self.class.fact_checker.send(m, self, fact) }
   end
 end
