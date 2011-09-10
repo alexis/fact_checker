8fce25250bfdae6a418ba6c612a4250c79d0fa75
Ivan Povalyukhin <ipoval@ya.ru>
2011-09-10 10:45:31 -0700

    simplified definition of true_fact_with_no_dependencies in test

--- spec/fact_checker_spec.rb
+++ spec/fact_checker_spec.rb
@@ -9,7 +9,7 @@ describe 'FactChecker' do
         include FactChecker
 
         def_fact :bare_fact
-        def_fact :true_fact_with_no_dependencies, :if => lambda { |o| !!o.object_id }
+        def_fact :true_fact_with_no_dependencies, :if => lambda { true }
         def_fact :false_fact_with_no_dependencies, :if => lambda { false }
         def_fact :true_fact_with_true_dependencies => :bare_fact, :if => lambda { true }
         def_fact :true_fact_with_false_dependencies => :false_fact_with_no_dependencies, :if => lambda { true }
